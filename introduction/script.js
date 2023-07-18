9b63ca2c893291a98bb25108b8016e2d90d882b7
dinesha17 <dineshkad17@gmail.com>
2023-07-18 12:25:59 +0530

    hello

---
+++ introduction/script.js
@@ -0,0 +1,49 @@
+alert("hi")
+console.log("Hello happy yers") 
+   //variables  are used to store data in cricket score example value
+    // variable   0 is initialization  
+    //dynamically type language
+    // variable - 8 bytes taken
+let score = 0 ;
+// assigment
+score = 10;
+console.log("score is"+score)
+let amount = 1000;
+console.log("your bill amount is RS" +amount)
+//binary value 1010
+const pointvalue = 3.14;
+// pointvalue =10
+console.log(pointvalue)
+
+// operators
+// arithmetic opertors:+ - *  ** / %  ++ --
+// assigment opertors: = += -= *= %= **=
+// // comparison opertors : == === != !== > >= <= ?
+// Infinity 
+// logical operators:&& || !
+// bitwise operators:& | ~ ^ << >> >>>
+
+
+// comparsion checking
+
+// checking in browers in console check to
+
+// string  '5'
+ a = '5' 
+ a == 5 
+ console.log(a)  
+// true == type and value 
+ b = '10'
+ b === 10
+ console.log(b)
+// false  === type one check
+
+// 0101 0111 0101
+a&b
+
+//user input
+// let name =prompt("what your name?");
+// console.log("hi",name)
+
+let tickets =prompt("what your tickets?");
+console.log("hi",tickets*20)
